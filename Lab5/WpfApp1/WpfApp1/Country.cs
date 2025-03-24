52dd89143a0489e603a95ce72fb683acbcf9d835
YakymivDanylo <dr.yakimiv@gmail.com>
2025-03-24 16:15:29 +0200

    lab 5 task1 done. Here is OOP with realistion of the class, with his methods, writing into txt file and reading it. Also interesting realisation of GUI. CAN BE USED IN COURSE WORK

---
+++ Lab5/WpfApp1/WpfApp1/Country.cs
@@ -0,0 +1,37 @@
+﻿namespace WpfApp1;
+
+public class Country
+{
+    private string name;
+    private string capital;
+    private int population;
+    private double area;
+    private string officialLanguage;
+    private string currency;
+    private string continent;
+
+    public string Name { get => name; set => name = value; }
+    public string Capital { get => capital; set => capital = value; }
+    public int Population { get => population; set => population = value; }
+    public double Area { get => area; set => area = value; }
+    public string OfficialLanguage { get => officialLanguage; set => officialLanguage = value; }
+    public string Currency { get => currency; set => currency = value; }
+    public string Continent { get => continent; set => continent = value; }
+
+    public Country() { }
+
+    public string GetCountryInfo()
+    {
+        return $"Країна: {name}\nСтолиця: {capital}\nНаселення: {population}\nПлоща: {area} км²\nОфіційна мова: {officialLanguage}\nВалюта: {currency}\nКонтинент: {continent}";
+    }
+
+    public double CalculatePopulationDensity()
+    {
+        return population / area;
+    }
+
+    public override string ToString()
+    {
+        return GetCountryInfo();
+    }
+}
---
+++ Lab5/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private Country country;
+    public MainWindow()
+    {
+        InitializeComponent();
+        country = new Country();
+    }
+    private void SaveData_Click(object sender, RoutedEventArgs e)
+    {
+        country.Name = txtName.Text;
+        country.Capital = txtCapital.Text;
+        country.Population = int.Parse(txtPopulation.Text);
+        country.Area = double.Parse(txtArea.Text);
+        country.OfficialLanguage = txtLanguage.Text;
+        country.Currency = txtCurrency.Text;
+        country.Continent = txtContinent.Text;
+
+        File.WriteAllText("country.txt", country.ToString());
+        MessageBox.Show("Дані збережено у файл country.txt", "Збереження", MessageBoxButton.OK, MessageBoxImage.Information);
+    }
+    private void ShowInfo_Click(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show(country.GetCountryInfo(), "Інформація про країну", MessageBoxButton.OK, MessageBoxImage.Information);
+    }
+
+    private void EstimateDensity_Click(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show($"Густота населення: {country.CalculatePopulationDensity():F2} осіб/км²", "Розрахунок густоти", MessageBoxButton.OK, MessageBoxImage.Information);
+    }
+}
